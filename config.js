8697ed5830cdbef8913f04b937b7021c979e22ba
xyz0yt <abhayp1282@gmail.com>
2024-12-24 19:20:01 +0100

    Create config.js
    Add config.js with API key and Channel ID

---
+++ config.js
@@ -0,0 +1,4 @@
+// config.js
+
+export const CHANNEL_ID = "UC42lGeyG5Psjr6M0W2_HTZg";  // Replace this with your actual YouTube Channel ID
+export const API_KEY = "AIzaSyBxZ9YyK6MB0S3lB8Xh9kgO27hg5LoZ3Og";  // Replace this with your actual YouTube Data API key
